b97b4e44e9a44251d7e8d6f902f853d72d36314c
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-06-15 16:00:59 +0300

    add

--- snacks/views.py
+++ snacks/views.py
@@ -4,5 +4,10 @@ from django.views.generic import ListView, DetailView
 # Create your views here.
 
 class SnacksListView(ListView):
-    template_name = 'List.html'
+    template_name = 'snack_list.html'
+    model = Snack
+
+
+class SnacksDetailView(DetailView):
+    template_name = "snacks_detail.html"
     model = Snack
